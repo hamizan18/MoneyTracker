cacd87a2e852ef55f5bd8d7ba8849e8827fc23d9
Jaann <hamizanputra321@gmail.com>
2025-09-21 00:41:40 +0700

    segini dulu deh

--- MySecondApp2/app/src/main/java/com/example/mysecondapp/MainActivity.java
+++ MySecondApp2/app/src/main/java/com/example/mysecondapp/MainActivity.java
@@ -2,36 +2,62 @@ package com.example.mysecondapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Size;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Button;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText inputUang;
-    Button btnSimpan;
-    TextView txtHasil;
-    int total = 0;
+    TextView txtHello, txtJgnklik;
+    Button btnUbah, btnTetap, btnReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        inputUang = findViewById(R.id.inputUang);
-        btnSimpan = findViewById(R.id.btnSimpan);
-        txtHasil = findViewById(R.id.txtHasil);
+        txtHello = findViewById(R.id.txtHello);
+        btnUbah = findViewById(R.id.btnUbah);
+        btnTetap = findViewById(R.id.btnTetap);
+        txtJgnklik = findViewById(R.id.txtJgnklik);
 
-        btnSimpan.setOnClickListener(new View.OnClickListener() {
+        btnUbah.setOnClickListener(new View.OnClickListener() {
+            boolean clicked = false;
+
             @Override
             public void onClick(View v) {
-                String input = inputUang.getText().toString();
-                if (!input.isEmpty()) {
-                    int uang = Integer.parseInt(input);
-                    total += uang;
-                    txtHasil.setText("Total: " + total);
-                    inputUang.setText(""); // kosongkan input lagi
+                if (!clicked){
+                    txtHello.setText("Teks berhasil diubah!");
+                    txtHello.setTextColor(Color.parseColor("#008000"));
+                    clicked = true;
+                } else {
+                    txtHello.setText("Hello Mijann dari Java Android!");
+                    txtHello.setTextColor(Color.parseColor("#000000"));
+                    clicked = false;
+                }
+            });
+        }
+        btnTetap.setOnClickListener(new View.OnClickListener() {
+            boolean clicked = false;
+
+            @Override
+            public void onClick(View v) {
+                if (!clicked) {
+                    txtJgnklik.setText("Kan dah dibilang jangan diklik");
+                    txtJgnklik.setTextColor(Color.parseColor("#ff0021"));
+                    txtJgnklik.setTypeface(null, Typeface.BOLD);
+                    txtJgnklik.setTextSize(25);
+                    clicked = true;
+                } else {
+                    txtJgnklik.setText("Jangan coba coba");
+                    txtJgnklik.setTextColor(Color.parseColor("#000000"));
+                    txtJgnklik.setTypeface(null, Typeface.NORMAL);
+                    txtJgnklik.setTextSize(15);
+                    clicked = false;
                 }
             }
         });
