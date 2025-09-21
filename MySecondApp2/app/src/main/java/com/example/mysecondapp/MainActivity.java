0582fd709a1dfe7cd4a2c31425b6cb8229521d9a
Jaann <hamizanputra321@gmail.com>
2025-09-21 10:16:56 +0700

    update tombol reset, center, tambah judul, sama Toast

--- MySecondApp2/app/src/main/java/com/example/mysecondapp/MainActivity.java
+++ MySecondApp2/app/src/main/java/com/example/mysecondapp/MainActivity.java
@@ -2,19 +2,28 @@ package com.example.mysecondapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Size;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txtHello, txtJgnklik;
     Button btnUbah, btnTetap, btnReset;
-
+    String[] kalimatRandom = {
+            "HAY TAYO",
+            "ALAMAWG",
+            "OPSEEHHH",
+            "MengSHIBALKAN",
+            "IYH",
+            "DIEM DIG",
+            "XiXIXI",
+            "RAWR",
+            "Aduhaaii"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         txtHello = findViewById(R.id.txtHello);
         btnUbah = findViewById(R.id.btnUbah);
         btnTetap = findViewById(R.id.btnTetap);
+        btnReset = findViewById(R.id.btnReset);
         txtJgnklik = findViewById(R.id.txtJgnklik);
 
         btnUbah.setOnClickListener(new View.OnClickListener() {
@@ -31,16 +41,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (!clicked){
-                    txtHello.setText("Teks berhasil diubah!");
-                    txtHello.setTextColor(Color.parseColor("#008000"));
+                    int indexRandom = (int)(Math.random() * kalimatRandom.length);
+                    txtHello.setText(kalimatRandom[indexRandom]);
+                    txtHello.setTextColor(getResources().getColor(R.color.pink));
+                    Toast.makeText(MainActivity.this, "Teks Berubah YEY!", Toast.LENGTH_SHORT).show();
                     clicked = true;
                 } else {
-                    txtHello.setText("Hello Mijann dari Java Android!");
+                    int indexRandom = (int)(Math.random() * kalimatRandom.length);
+                    txtHello.setText(kalimatRandom[indexRandom]);
                     txtHello.setTextColor(Color.parseColor("#000000"));
+                    Toast.makeText(MainActivity.this, "Teks Berubah YEY!", Toast.LENGTH_SHORT).show();
                     clicked = false;
                 }
-            });
-        }
+            }
+        });
         btnTetap.setOnClickListener(new View.OnClickListener() {
             boolean clicked = false;
 
@@ -48,17 +62,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (!clicked) {
                     txtJgnklik.setText("Kan dah dibilang jangan diklik");
-                    txtJgnklik.setTextColor(Color.parseColor("#ff0021"));
+                    txtJgnklik.setTextColor(getResources().getColor(R.color.merah));
                     txtJgnklik.setTypeface(null, Typeface.BOLD);
                     txtJgnklik.setTextSize(25);
+                    Toast.makeText(MainActivity.this, "Teks Berubah YEY!", Toast.LENGTH_SHORT).show();
                     clicked = true;
                 } else {
                     txtJgnklik.setText("Jangan coba coba");
                     txtJgnklik.setTextColor(Color.parseColor("#000000"));
                     txtJgnklik.setTypeface(null, Typeface.NORMAL);
                     txtJgnklik.setTextSize(15);
+                    Toast.makeText(MainActivity.this, "Teks Berubah YEY!", Toast.LENGTH_SHORT).show();
                     clicked = false;
                 }
+            }
+        });
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                    txtHello.setText("Hello Mijann dari Java Android!");
+                    txtHello.setTextColor(Color.parseColor("#000000"));
+                    txtJgnklik.setText("Jangan coba coba");
+                    txtJgnklik.setTextSize(15);
+                    txtJgnklik.setTextColor(Color.parseColor("#000000"));
+                    txtJgnklik.setTypeface(null, Typeface.NORMAL);
+                    Toast.makeText(MainActivity.this, "Direset.", Toast.LENGTH_SHORT).show();
             }
         });
     }
