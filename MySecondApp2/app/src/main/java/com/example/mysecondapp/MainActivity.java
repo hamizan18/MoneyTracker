97174b3eaf100cb733da7b9139a1da37540989a6
Jaann <hamizanputra321@gmail.com>
2025-09-21 11:07:53 +0700

    Quest: input - output clear!

--- MySecondApp2/app/src/main/java/com/example/mysecondapp/MainActivity.java
+++ MySecondApp2/app/src/main/java/com/example/mysecondapp/MainActivity.java
@@ -8,11 +8,13 @@ import android.widget.Button;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.widget.Toast;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtHello, txtJgnklik;
-    Button btnUbah, btnTetap, btnReset;
+    TextView txtHello, txtJgnklik, teksBaru;
+    Button btnUbah, btnTetap, btnReset, btnSetCustom;
+    EditText inputCustom;
     String[] kalimatRandom = {
             "HAY TAYO",
             "ALAMAWG",
@@ -34,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         btnTetap = findViewById(R.id.btnTetap);
         btnReset = findViewById(R.id.btnReset);
         txtJgnklik = findViewById(R.id.txtJgnklik);
+        btnSetCustom = findViewById(R.id.btnSetCustom);
+        inputCustom = findViewById(R.id.inputCustom);
+        teksBaru = findViewById(R.id.teksBaru);
 
         btnUbah.setOnClickListener(new View.OnClickListener() {
             boolean clicked = false;
@@ -86,6 +91,18 @@ public class MainActivity extends AppCompatActivity {
                     txtJgnklik.setTextColor(Color.parseColor("#000000"));
                     txtJgnklik.setTypeface(null, Typeface.NORMAL);
                     Toast.makeText(MainActivity.this, "Direset.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnSetCustom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputTeks = inputCustom.getText().toString();
+                if (!inputTeks.isEmpty()) {
+                    teksBaru.setText("Inputan: " + inputTeks);
+                    Toast.makeText(MainActivity.this, "Teks diinput!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Inputan Kosong!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
