cefb6a400ba4c20a95274e0ad24da2827c2b2b4d
Jaann <hamizanputra321@gmail.com>
2025-09-21 13:07:04 +0700

    nambah animasi, background, sama counterr

--- MySecondApp2/app/src/main/java/com/example/mysecondapp/MainActivity.java
+++ MySecondApp2/app/src/main/java/com/example/mysecondapp/MainActivity.java
@@ -9,12 +9,21 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.widget.Toast;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txtHello, txtJgnklik, teksBaru;
     Button btnUbah, btnTetap, btnReset, btnSetCustom;
     EditText inputCustom;
+    LinearLayout rootLayout;
+    int[] warnaRandom = {
+            Color.BLUE,
+            Color.GREEN,
+            Color.RED,
+            Color.parseColor("#FFA500")
+    };
+    int counter = 0;
     String[] kalimatRandom = {
             "HAY TAYO",
             "ALAMAWG",
@@ -39,24 +48,30 @@ public class MainActivity extends AppCompatActivity {
         btnSetCustom = findViewById(R.id.btnSetCustom);
         inputCustom = findViewById(R.id.inputCustom);
         teksBaru = findViewById(R.id.teksBaru);
+        rootLayout = findViewById(R.id.rootLayout);
 
         btnUbah.setOnClickListener(new View.OnClickListener() {
             boolean clicked = false;
 
             @Override
             public void onClick(View v) {
+                int indexRandom = (int)(Math.random() * kalimatRandom.length);
+                int warnaAcak = warnaRandom[(int)(Math.random()* warnaRandom.length)];
                 if (!clicked){
-                    int indexRandom = (int)(Math.random() * kalimatRandom.length);
+                    rootLayout.setBackgroundColor(warnaAcak);
+                    txtHello.animate().rotationBy(360).setDuration(500);
                     txtHello.setText(kalimatRandom[indexRandom]);
-                    txtHello.setTextColor(getResources().getColor(R.color.pink));
-                    Toast.makeText(MainActivity.this, "Teks Berubah YEY!", Toast.LENGTH_SHORT).show();
+                    txtHello.setTextColor(warnaAcak);
                     clicked = true;
+                    counter++;
+                    Toast.makeText(MainActivity.this, "Jumlah klik: " + counter, Toast.LENGTH_SHORT).show();
                 } else {
-                    int indexRandom = (int)(Math.random() * kalimatRandom.length);
+                    txtHello.animate().rotationBy(360).setDuration(500);
                     txtHello.setText(kalimatRandom[indexRandom]);
                     txtHello.setTextColor(Color.parseColor("#000000"));
-                    Toast.makeText(MainActivity.this, "Teks Berubah YEY!", Toast.LENGTH_SHORT).show();
                     clicked = false;
+                    counter++;
+                    Toast.makeText(MainActivity.this, "Jumlah klik: " + counter, Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -70,15 +85,17 @@ public class MainActivity extends AppCompatActivity {
                     txtJgnklik.setTextColor(getResources().getColor(R.color.merah));
                     txtJgnklik.setTypeface(null, Typeface.BOLD);
                     txtJgnklik.setTextSize(25);
-                    Toast.makeText(MainActivity.this, "Teks Berubah YEY!", Toast.LENGTH_SHORT).show();
                     clicked = true;
+                    counter++;
+                    Toast.makeText(MainActivity.this, "Jumlah klik: " + counter, Toast.LENGTH_SHORT).show();
                 } else {
                     txtJgnklik.setText("Jangan coba coba");
                     txtJgnklik.setTextColor(Color.parseColor("#000000"));
                     txtJgnklik.setTypeface(null, Typeface.NORMAL);
                     txtJgnklik.setTextSize(15);
-                    Toast.makeText(MainActivity.this, "Teks Berubah YEY!", Toast.LENGTH_SHORT).show();
                     clicked = false;
+                    counter++;
+                    Toast.makeText(MainActivity.this, "Jumlah klik: " + counter, Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -90,14 +107,19 @@ public class MainActivity extends AppCompatActivity {
                     txtJgnklik.setTextSize(15);
                     txtJgnklik.setTextColor(Color.parseColor("#000000"));
                     txtJgnklik.setTypeface(null, Typeface.NORMAL);
-                    Toast.makeText(MainActivity.this, "Direset.", Toast.LENGTH_SHORT).show();
+                    counter = 0;
+                    Toast.makeText(MainActivity.this, "Semua direset.", Toast.LENGTH_SHORT).show();
             }
         });
         btnSetCustom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String inputTeks = inputCustom.getText().toString();
-                if (!inputTeks.isEmpty()) {
+                if (inputTeks.length() > 10) {
+                    Toast.makeText(MainActivity.this, "Teks kepanjangan!", Toast.LENGTH_SHORT).show();
+                } else if (!inputTeks.isEmpty()) {
+                    int indexWarna = (int)(Math.random() * warnaRandom.length);
+                    teksBaru.setTextColor(warnaRandom[indexWarna]);
                     teksBaru.setText("Inputan: " + inputTeks);
                     Toast.makeText(MainActivity.this, "Teks diinput!", Toast.LENGTH_SHORT).show();
                 } else {
